29a8d7101056d048c19d0d5da5e67155ffd5265e
fabio bispo <fabiobispoantonio@hotmail.com>
2021-05-06 20:38:51 -0300

    H2

--- src/main/java/com/fabio_antonio/curso/entities/User.java
+++ src/main/java/com/fabio_antonio/curso/entities/User.java
@@ -2,123 +2,130 @@ package com.fabio_antonio.curso.entities;
 
 import java.io.Serializable;
 
-public class User implements Serializable	{
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-		private Long id;
-		private String name;
-		private String email;
-		private String phone;
-		private String password;
-		
-		public User() {
-			
-		}
 
-		public User(Long id, String name, String email, String phone, String password) {
-			super();
-			this.id = id;
-			this.name = name;
-			this.email = email;
-			this.phone = phone;
-			this.password = password;
-		}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
 
-		public Long getId() {
-			return id;
-		}
+	public User() {
 
-		public void setId(Long id) {
-			this.id = id;
-		}
+	}
 
-		public String getName() {
-			return name;
-		}
+	public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
 
-		public void setName(String name) {
-			this.name = name;
-		}
+	public Long getId() {
+		return id;
+	}
 
-		public String getEmail() {
-			return email;
-		}
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-		public void setEmail(String email) {
-			this.email = email;
-		}
+	public String getName() {
+		return name;
+	}
 
-		public String getPhone() {
-			return phone;
-		}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-		public void setPhone(String phone) {
-			this.phone = phone;
-		}
+	public String getEmail() {
+		return email;
+	}
 
-		public String getPassword() {
-			return password;
-		}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-		public void setPassword(String password) {
-			this.password = password;
-		}
+	public String getPhone() {
+		return phone;
+	}
 
-		@Override
-		public String toString() {
-			return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password="
-					+ password + "]";
-		}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((email == null) ? 0 : email.hashCode());
-			result = prime * result + ((id == null) ? 0 : id.hashCode());
-			result = prime * result + ((name == null) ? 0 : name.hashCode());
-			result = prime * result + ((password == null) ? 0 : password.hashCode());
-			result = prime * result + ((phone == null) ? 0 : phone.hashCode());
-			return result;
-		}
+	public String getPassword() {
+		return password;
+	}
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			User other = (User) obj;
-			if (email == null) {
-				if (other.email != null)
-					return false;
-			} else if (!email.equals(other.email))
-				return false;
-			if (id == null) {
-				if (other.id != null)
-					return false;
-			} else if (!id.equals(other.id))
-				return false;
-			if (name == null) {
-				if (other.name != null)
-					return false;
-			} else if (!name.equals(other.name))
-				return false;
-			if (password == null) {
-				if (other.password != null)
-					return false;
-			} else if (!password.equals(other.password))
-				return false;
-			if (phone == null) {
-				if (other.phone != null)
-					return false;
-			} else if (!phone.equals(other.phone))
-				return false;
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password=" + password
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
 			return true;
-		}
-		
-		
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phone == null) {
+			if (other.phone != null)
+				return false;
+		} else if (!phone.equals(other.phone))
+			return false;
+		return true;
+	}
 
 }
