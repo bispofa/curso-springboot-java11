458f4748896880117b871cecbe9152a07f8968e8
fabio bispo <fabiobispoantonio@hotmail.com>
2021-05-08 10:25:35 -0300

    312

--- src/main/java/com/fabio_antonio/curso/repositories/UserRepository.java
+++ src/main/java/com/fabio_antonio/curso/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.fabio_antonio.curso.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.fabio_antonio.curso.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 
