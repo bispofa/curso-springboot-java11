f4d8872dfcea68b896f07485ad4e15b410922c36
fabio bispo <fabiobispoantonio@hotmail.com>
2021-05-23 14:24:26 -0300

    entite order part 3

--- src/main/java/com/fabio_antonio/curso/entities/Order.java
+++ src/main/java/com/fabio_antonio/curso/entities/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fabio_antonio.curso.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
@@ -24,6 +25,9 @@ public class Order implements Serializable{
 	private Long id;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH;mm:ss'Z'",timezone = "GMT")
 	private Instant moment;
+	private Integer orderStatus;
+	
+	
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private User client;
@@ -32,11 +36,22 @@ public class Order implements Serializable{
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public Long getId() {
