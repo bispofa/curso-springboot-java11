0bdd956b66a9b2dae971ffef7fc10d27d1e217a2
fabio bispo <fabiobispoantonio@hotmail.com>
2021-05-23 14:46:56 -0300

    category

--- src/main/java/com/fabio_antonio/curso/config/TestConfig.java
+++ src/main/java/com/fabio_antonio/curso/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.fabio_antonio.curso.entities.Category;
 import com.fabio_antonio.curso.entities.Order;
 import com.fabio_antonio.curso.entities.User;
 import com.fabio_antonio.curso.entities.enums.OrderStatus;
+import com.fabio_antonio.curso.repositories.CategoryRepository;
 import com.fabio_antonio.curso.repositories.OrderRepository;
 import com.fabio_antonio.curso.repositories.UserRepository;
 
@@ -22,6 +24,8 @@ public class TestConfig implements CommandLineRunner{
 	private UserRepository userRepository;
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -33,8 +37,12 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2); 
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
 		
+		Category cat1 = new Category(null, "Electronics"); 
+		Category cat2 = new Category(null, "Books"); 
+		Category cat3 = new Category(null, "Computers"); 
 		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 	}
 }
---
+++ src/main/java/com/fabio_antonio/curso/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.fabio_antonio.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fabio_antonio.curso.entities.Category;
+import com.fabio_antonio.curso.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+
+	public List<Category> findAll(){
+		return  repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
