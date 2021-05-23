c82b24a8377ca5fb06f508111f61baaa41efd755
fabio bispo <fabiobispoantonio@hotmail.com>
2021-05-23 12:08:37 -0300

    entite order

--- src/main/java/com/fabio_antonio/curso/entities/User.java
+++ src/main/java/com/fabio_antonio/curso/entities/User.java
@@ -1,13 +1,18 @@
 package com.fabio_antonio.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -19,9 +24,16 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 
+	}
+
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -71,6 +83,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
