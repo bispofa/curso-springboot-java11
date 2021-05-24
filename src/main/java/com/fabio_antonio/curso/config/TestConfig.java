34c3668f1932b7926671b48b1fed0c479b958623
bispofa <fabiobispoantonio@hotmail.com>
2021-05-23 21:33:14 -0300

    payment

--- src/main/java/com/fabio_antonio/curso/config/TestConfig.java
+++ src/main/java/com/fabio_antonio/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.fabio_antonio.curso.entities.Category;
 import com.fabio_antonio.curso.entities.Order;
 import com.fabio_antonio.curso.entities.OrderItem;
+import com.fabio_antonio.curso.entities.Payment;
 import com.fabio_antonio.curso.entities.Product;
 import com.fabio_antonio.curso.entities.User;
 import com.fabio_antonio.curso.entities.enums.OrderStatus;
@@ -76,5 +77,8 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		orderRepository.saveAll(Arrays.asList(o1));
 	}
 }
